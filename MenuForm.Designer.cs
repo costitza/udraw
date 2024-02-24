920992d3bb537be3cfdf66058710a21823de291e
Raul Ababei <ababeiraul13@gmail.com>
2024-02-24 17:14:36 +0200

    commited changes

--- MenuForm.Designer.cs
+++ MenuForm.Designer.cs
@@ -28,12 +28,117 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MenuForm));
+            this.panelAction = new System.Windows.Forms.Panel();
+            this.panelBoards = new System.Windows.Forms.Panel();
+            this.buttonBoards = new System.Windows.Forms.Button();
+            this.buttonDrawings = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.panelAction.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelAction
+            // 
+            this.panelAction.BackColor = System.Drawing.Color.Tan;
+            this.panelAction.Controls.Add(this.panel1);
+            this.panelAction.Controls.Add(this.buttonDrawings);
+            this.panelAction.Controls.Add(this.buttonBoards);
+            this.panelAction.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelAction.Location = new System.Drawing.Point(0, 0);
+            this.panelAction.Name = "panelAction";
+            this.panelAction.Size = new System.Drawing.Size(235, 511);
+            this.panelAction.TabIndex = 0;
+            // 
+            // panelBoards
+            // 
+            this.panelBoards.BackColor = System.Drawing.Color.Bisque;
+            this.panelBoards.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelBoards.Location = new System.Drawing.Point(235, 0);
+            this.panelBoards.Name = "panelBoards";
+            this.panelBoards.Size = new System.Drawing.Size(558, 511);
+            this.panelBoards.TabIndex = 1;
+            // 
+            // buttonBoards
+            // 
+            this.buttonBoards.BackColor = System.Drawing.Color.Peru;
+            this.buttonBoards.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonBoards.Location = new System.Drawing.Point(44, 40);
+            this.buttonBoards.Name = "buttonBoards";
+            this.buttonBoards.Size = new System.Drawing.Size(135, 63);
+            this.buttonBoards.TabIndex = 0;
+            this.buttonBoards.Text = "Your recent boards";
+            this.buttonBoards.UseVisualStyleBackColor = false;
+            // 
+            // buttonDrawings
+            // 
+            this.buttonDrawings.BackColor = System.Drawing.Color.Peru;
+            this.buttonDrawings.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDrawings.Location = new System.Drawing.Point(44, 125);
+            this.buttonDrawings.Name = "buttonDrawings";
+            this.buttonDrawings.Size = new System.Drawing.Size(135, 63);
+            this.buttonDrawings.TabIndex = 1;
+            this.buttonDrawings.Text = "Drawing models";
+            this.buttonDrawings.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonDelete);
+            this.panel1.Controls.Add(this.buttonAdd);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 323);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(235, 188);
+            this.panel1.TabIndex = 2;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.BackColor = System.Drawing.Color.Lime;
+            this.buttonAdd.Font = new System.Drawing.Font("Showcard Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAdd.Location = new System.Drawing.Point(34, 34);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(170, 44);
+            this.buttonAdd.TabIndex = 0;
+            this.buttonAdd.Text = "New board";
+            this.buttonAdd.UseVisualStyleBackColor = false;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.BackColor = System.Drawing.Color.Red;
+            this.buttonDelete.Font = new System.Drawing.Font("Showcard Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDelete.Location = new System.Drawing.Point(34, 106);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(170, 44);
+            this.buttonDelete.TabIndex = 1;
+            this.buttonDelete.Text = "Delete Board";
+            this.buttonDelete.UseVisualStyleBackColor = false;
+            // 
+            // MenuForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "MenuForm";
+            this.ClientSize = new System.Drawing.Size(793, 511);
+            this.Controls.Add(this.panelBoards);
+            this.Controls.Add(this.panelAction);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MenuForm";
+            this.Text = "Udraw";
+            this.panelAction.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelAction;
+        private System.Windows.Forms.Panel panelBoards;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonDrawings;
+        private System.Windows.Forms.Button buttonBoards;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonAdd;
     }
 }
