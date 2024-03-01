98fb47009369302966f00e1b41753328c68d22d4
Raul Ababei <ababeiraul13@gmail.com>
2024-03-01 16:52:25 +0200

    added all shapes

---
+++ Shapes/Ellipse.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Udraw.Shapes
+{
+    class EllipseShape : Shape
+    {
+        private Point startPoint;
+        private Point endPoint;
+        private Color color;
+        private int width;
+
+        public EllipseShape(Point startPoint, Point endPoint, Color color, int width)
+        {
+            this.startPoint = startPoint;
+            this.endPoint = endPoint;
+            this.color = color;
+            this.width = width;
+        }
+
+        public override void Draw(Graphics g)
+        {
+            using (Pen pen = new Pen(color, width))
+            {
+                int halfWidth = Math.Abs(endPoint.X - startPoint.X) / 2;
+                int halfHeight = Math.Abs(endPoint.Y - startPoint.Y) / 2;
+
+                int centerX = Math.Min(startPoint.X, endPoint.X) + halfWidth;
+                int centerY = Math.Min(startPoint.Y, endPoint.Y) + halfHeight;
+
+                g.DrawEllipse(pen, centerX - halfWidth, centerY - halfHeight, 2 * halfWidth, 2 * halfHeight);
+            }
+        }
+    }
+
+}
---
+++ Shapes/Square.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Udraw.Shapes
+{
+    class SquareShape : Shape
+    {
+        private Point startPoint;
+        private Point endPoint;
+        private Color color;
+        private int width;
+
+        public SquareShape(Point startPoint, Point endPoint, Color color, int width)
+        {
+            this.startPoint = startPoint;
+            this.endPoint = endPoint;
+            this.color = color;
+            this.width = width;
+        }
+
+        public override void Draw(Graphics g)
+        {
+            using (Pen pen = new Pen(color, width))
+            {
+                
+                int width = Math.Abs(endPoint.X - startPoint.X);
+                int height = Math.Abs(endPoint.Y - startPoint.Y);
+
+                
+                int size = Math.Min(width, height);
+
+                
+                int x = Math.Min(startPoint.X, endPoint.X);
+                int y = Math.Min(startPoint.Y, endPoint.Y);
+
+                Rectangle rect = new Rectangle(x, y, size, size);
+                g.DrawRectangle(pen, rect);
+            }
+        }
+    }
+}
